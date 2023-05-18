f4aecdf56b41f65ca23b04e63c53ce6ecd51e1c4
ti_cn <ti_cn@icloud.com>
2023-05-18 15:01:06 +0800

    idea

---
+++ src/main/java/com/hezhangqi/echo/utils/FileTools.java
@@ -0,0 +1,16 @@
+/*
+ * @Author: ti_cn ti_cn@icloud.com
+ * @Date: 2023-05-15 16:17:22
+ * @LastEditors: ti_cn ti_cn@icloud.com
+ * @LastEditTime: 2023-05-15 16:34:02
+ * @FilePath: /xxl-job-master/Users/hezhangqi/ehco_station/tinn/echo/src/main/java/com/hezhangqi/echo/utils/FileTools.java
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package com.hezhangqi.echo.utils;
+
+public class FileTools {
+    //实现文件上传功能
+    public static void uploadFile() {
+        //TODO:实现文件上传功能
+    }
+}
