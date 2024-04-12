7018c67d4bd02dda6ed499fc619ada09372f1a66
ti_cn <ti_cn@icloud.com>
2024-04-12 14:44:54 +0800

    240123

---
+++ src/main/java/com/hezhangqi/echo/config/TestConfig.java
@@ -0,0 +1,9 @@
+package com.hezhangqi.echo.config;
+
+/**
+ * @author hezhangqi
+ * @description myclass
+ * @date 2024/3/8 15:14:56
+ */
+public class TestConfig {
+}
--- src/test/java/com/hezhangqi/echo/EchoApplicationTests.java
+++ src/test/java/com/hezhangqi/echo/EchoApplicationTests.java
@@ -6,9 +6,7 @@ package com.hezhangqi.echo;
  */
 
 import com.hezhangqi.echo.controller.CallBackTestController;
-import com.hezhangqi.echo.enums.datatype.StatusEnum;
 import com.hezhangqi.echo.pojo.dto.Redhat002;
-import com.hezhangqi.echo.pojo.model.Recv001;
 import com.hezhangqi.echo.service.CallBackTestService;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.Cell;
@@ -49,35 +47,16 @@ class EchoApplicationTests {
     private final Map<String,Object> cache = new HashMap<>();
     @Test
     void contextLoads() {
-        // int i = 1;
-        // boolean in = StatusEnum.isIn("9999");
-        // System.out.println(in);
-        // StatusEnum statusEnum = StatusEnum.get("0000");
-        // System.out.println(statusEnum.getId());
-        // //获得枚举类的所有枚举值
-        // StatusEnum[] values = StatusEnum.values();
-        // for (StatusEnum value : values) {
-        //     System.out.println(value);
-        // }
-        Recv001 recv001 = new Recv001("", StatusEnum.SUCCESS);
-        boolean equals = StringUtils.equals(recv001.getId(), "");
-        System.out.println(equals);
+
     }
 
     //新写一个测试方法
     @Test
     void test() {
-       String s = "123#456#789";
-         String[] split = s.split("#",-1);
-         StringBuffer s1 = new StringBuffer();
-        for (String value : split) {
-            s1.append(String.format("\t%s,", value));
-        }
-
-            System.out.println(s1);
-
-        String s2 = s1 + "1";
-
+        Redhat002 redhat002 = new Redhat002();
+        redhat002.setAge("1");
+        boolean contains = redhat002.getName().contains("1");
+        System.out.println(contains);
 
 
     }
@@ -217,6 +196,20 @@ class EchoApplicationTests {
 //                System.out.println(line);
 //            }
 //            Files.write(file,lines);
+
+
+    }
+
+    @Test
+    void test8() {
+        String s = "727090126203653515";
+        long l = Long.valueOf(s);
+        long l1 = l % (300);
+        System.out.println(l1);
+    }
+    //获取本机的mac地址
+    @Test
+    void test9() {
 
 
     }
